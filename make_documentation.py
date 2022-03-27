75eb7d7055be0090200606cfe2070a0ad8f94385
oygo <86785290+oygo@users.noreply.github.com>
2022-03-27 22:01:24 +0200

    Try out jupyter notebooks

--- make_documentation.py
+++ make_documentation.py
@@ -7,10 +7,22 @@ notes_rel = '../../Notes/Code Snippets/'
 notes_abs = os.path.join(os.getcwd(), notes_rel)
 os.chdir(notes_abs)
 
-print(os.listdir()) # returns list
+p = os.listdir() # returns list
+p.remove('.obsidian')
+for i in p:
+    if os.path.isdir(i): # i is a directory
+        print(f'directory name: {i}')
+    else: # i is a file
+        print(f'file name: {i}')
 
-file = 'Functions  - Lambda expressions.md'
 
-shutil.copy2('Functions  - Lambda expressions.md', jekyll_abs) # target filename is /dst/dir/file.ext
+#print(contents)
+# print(contents[0])
+
+#file = 'Functions  - Lambda expressions.md'
+
+#shutil.copy2('Functions  - Lambda expressions.md', jekyll_abs) # target filename is /dst/dir/file.ext
 
 # os. 'Functions  - Lambda expressions.md'
+
+
